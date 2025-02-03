749cdb7cc217e7debea9a8b85f1a193b6b4a2c62
nourlouhaichi <Nour.Louhaichi@esprit.tn>
2025-02-03 17:56:12 +0100

    commit 1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Combien de chiffres voulez-vous additionner ? ");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] chiffres = new double[n];
+            double somme = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Entrez le chiffre {i + 1}: ");
+                chiffres[i] = double.Parse(Console.ReadLine());
+                somme += chiffres[i];
+            }
+
+            Console.WriteLine($"La somme des chiffres est: {somme}");
+        }
+    }
+}
